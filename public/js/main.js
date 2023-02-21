eae8202824eefd1cd9cc02697fd3e53d2838b75a
Julio Gazoli <julio_gazoli@hotmail.com>
2023-02-21 10:38:40 -0300

    game over com eventos

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,26 @@ var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
+campo.on("input",function() {
 	var conteudo = campo.val();
 
 	var qtdPalavras = conteudo.split(/\S+/).length - 1;
 	$("#contador-palavras").text(qtdPalavras);
 
-	var qtdCaracteres = conteudo.length;
+	// Retira os espaços da String
+	var conteudoSemEspaco = conteudo.replace(/\s+/g,'');
+	var qtdCaracteres = conteudoSemEspaco.length;
 	$("#contador-caracteres").text(qtdCaracteres);
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus",function(){
+	var cronometroID = setInterval(function(){
+		tempoRestante--;
+		$("#tempo-digitacao").text(tempoRestante);
+		if(tempoRestante < 1){
+			campo.attr("disabled",true);
+			clearInterval(cronometroID);
+		}
+	},1000);
 })
