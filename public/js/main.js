15a630b4cfa0cfd737e31b167dff9f08c5cdf043
Julio Gazoli <julio_gazoli@hotmail.com>
2023-02-22 08:46:41 -0300

    adicionando icones

--- public/js/main.js
+++ public/js/main.js
@@ -39,7 +39,7 @@ function inicializaCronometro() {
 			if(tempoRestante < 1){
 				campo.attr("disabled",true);
 				clearInterval(cronometroID);
-				campo.addClass("campo-desativado");
+				campo.toggleClass("campo-desativado");
 				$("#botao-reiniciar").attr("disabled",false);
 			}
 		},1000);
@@ -53,4 +53,5 @@ function reiniciaJogo() {
 	$("#contador-caracteres").text("0");
 	$("#tempo-digitacao").text(tempoInicial);
 	inicializaCronometro();
+	campo.toggleClass("campo-desativado");
 }
