5e650bc9cb301f7dba1f9875db2a603ee7371724
Julio Gazoli <julio_gazoli@hotmail.com>
2023-02-25 10:10:23 -0300

    adicionando eventos nas linhas do placar

--- public/js/main.js
+++ public/js/main.js
@@ -83,21 +83,37 @@ function inserePlacar(){
 	var corpoTabela = $(".placar").find("tbody");
 	var usuario = "Julio";
 	var numPalavras = $("#contador-palavras").text();
-	var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>";
 
-	var linha = "<tr>"+
-					"<td>"+ usuario + "</td>"+
-					"<td>"+ numPalavras + "</td>"+
-					"<td>"+ botaoRemover + "</td>"+
-				"</tr";
+	var linha = novaLinha(usuario,numPalavras);
+	linha.find(".botao-remover").click(removeLinha);
 
 	corpoTabela.prepend(linha);
 }
 
-$(".botao-remover").click(function(event){
+function novaLinha(usuario,palavras){
+	var linha = $("<tr>");
+	var colunaUsuario = $("<td>").text(usuario);
+	var colunaPalavras = $("<td>").text(palavras);
+	var colunaRemover = $("<td>");
+	
+	var link = $("<a>").addClass("botao-remover").attr("href","#");
+	var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+	link.append(icone);
+
+	colunaRemover.append(link);
+
+	linha.append(colunaUsuario);
+	linha.append(colunaPalavras);
+	linha.append(colunaRemover);
+
+	return linha;
+}
+
+function removeLinha(){
 	event.preventDefault();
 	$(this).parent().parent().remove();
-})
+}
 
 function reiniciaJogo(){
 	campo.attr("disabled",false);
