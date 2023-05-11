cf972d3aac82527bbe3e6cda4536dfc3848e85d0
Julio Gazoli <julio_gazoli@hotmail.com>
2023-05-11 16:41:58 -0300

    Arrumando tempo e texto

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,11 @@ $(function(){
 	$("#botao-reiniciar").click(reiniciaJogo);
 });
 
+function atualizaTempoInicial(tempo) {
+	tempoInicial = tempo;
+	$("#tempo-digitacao").text(tempo);
+}
+
 function atualizaTamanhoFrase() {
 	var frase = $(".frase").text();
 	var numPalavras = frase.split(" ").length;
@@ -31,8 +36,8 @@ function inicializaContadores() {
 }
 
 function inicializaCronometro() {
-	var tempoRestante = $("#tempo-digitacao").text();
 	campo.one("focus",function(){
+		var tempoRestante = $("#tempo-digitacao").text();
 		var cronometroID = setInterval(function(){
 			$("#botao-reiniciar").attr("disabled",true);
 			tempoRestante--;
@@ -53,8 +58,8 @@ function finalizaJogo(){
 }
 
 function inicializaMarcadores(){
-	var frase = $(".frase").text();
 	campo.on("input",function(){
+		var frase = $(".frase").text();
 		var digitado = campo.val();
 		var comparavel = frase.substr(0,digitado.length);
 		/*
