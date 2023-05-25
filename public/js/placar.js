a2e2ff8bd4db0001abd41641c4695c6c5991246a
Julio Gazoli <julio_gazoli@hotmail.com>
2023-05-25 15:56:47 -0300

    Enviando dados com POST - Introdução

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,4 +1,5 @@
 $("#botao-placar").click(mostraPlacar);
+$("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar(){
 	var corpoTabela = $(".placar").find("tbody");
@@ -53,4 +54,8 @@ function removeLinha(event){
 
 function mostraPlacar(){
 	$(".placar").stop().slideToggle(600);
+}
+
+function sincronizaPlacar() {
+	console.log("sincronizando")
 }
